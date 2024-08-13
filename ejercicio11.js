bb54febae0b033310c682f3d1b884a49e81f4003
YaredSD <yared.santana@icloud.com>
2024-08-13 11:16:08 +0100

    12.0
    Hasta el 11 realizados y corregidos

--- ejercicio11.js
+++ ejercicio11.js
@@ -1,15 +1,12 @@
 const mixedElements = [6, 1, 'Marvel', 1, 'hamburguesa', '10', 'Prometeo', 8, 'Hola mundo'];
 function averageWord(list) {
-  
-  return
+  let Total = 0;
+  for (let index = 0; index < list.length; index++) {
+    if (typeof list[index] === "string") {
+      Total = Total + list[index].length;
+    } else {
+      Total = Total + list[index];
+    }
+  }
+  return Total
 }
-
-averageWord(mixedElements);
-
-/*
-Calcular promedio mezclado: Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume.
-
-Es un poco locura, pero podremos ejercitar nuestra lógica con este ejercicio.
-
-Puedes usar este array para probar tu función:
-*/
